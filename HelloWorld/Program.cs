eba573e9ce5de7569cbb1c1787d1f292b0b4534a
J D <zathrath03@gmail.com>
2022-12-26 13:55:24 -0800

    Message output challenge

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -30,7 +30,29 @@ namespace HelloWorld
             var message = "Hello World!";
             // message = 0; Cannot implicitly convert type 'int' to 'string'
             Console.WriteLine(message);
-            
+
+            Console.WriteLine("==========================================================");
+            // Write code in the .NET Editor to display a message
+            // When you're finished, the message should resemble the following output:
+            // Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
+            // Store Bob, 3, and 34.4 in variables
+            // These variables should be given names that reflect their purpose.
+            // Make sure you select the correct data type for each of the variables based on the type of data it will hold.
+            // Finally, you'll combine the variables with literal strings passed into a series of Console.Write() commands to form the complete message.
+
+            string name = "Bob";
+            byte numberOfMessages = 3;
+            float temperature = 34.4f;
+
+            Console.Write("Hello, ");
+            Console.Write(name);
+            Console.Write("! You have ");
+            Console.Write(numberOfMessages);
+            Console.Write(" messages in your inbox. The temperature is ");
+            Console.Write(temperature);
+            Console.Write(" celsius.");
+
+
             Console.ReadKey();
         }
     }
