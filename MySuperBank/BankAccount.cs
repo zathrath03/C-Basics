debba85d8b247d41f2c9583a84cb5b02aa370c01
J D <zathrath03@gmail.com>
2022-12-30 14:27:15 -0800

    Creating classes

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+
+        public string Number { get;  }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public BankAccount(string ownerName, decimal initialBalance)
+        {
+            Owner = ownerName;
+            Balance = initialBalance;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note) { }
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("John", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with a balance of {account.Balance}");
+        }
+    }
+}
