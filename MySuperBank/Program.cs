02aaf3f3c6b6d16f5793e5489f1967fd4ca2f506
J D <zathrath03@gmail.com>
2022-12-30 15:38:57 -0800

    Cleanup unused usings and remove args parameter from Main()

--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MySuperBank
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             var account = new BankAccount("John", 10000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with a balance of {account.Balance}");
