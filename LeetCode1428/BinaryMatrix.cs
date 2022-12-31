c10ae0716c13760ce86e2db1ab8618a2ebc3a08f
J D <zathrath03@gmail.com>
2022-12-31 12:44:37 -0800

    LeetCode Solution

---
+++ LeetCode1428/BinaryMatrix.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode1428
+{
+    internal class BinaryMatrix
+    {
+        public int Get(int row, int col)
+        {
+            return 0;
+        }
+
+        public List<int> Dimensions()
+        {
+            return new List<int>() {0, 0};
+        }
+    }
+}
---
+++ LeetCode1428/Solution.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCode1428
+{
+    internal class Solution
+    {
+        private const int MaxMatrixLength = 100;
+        private BinaryMatrix _binaryMatrix;
+        private int _rows;
+        private int _cols;
+        private int _leftmostColumnWithOne = MaxMatrixLength + 1;
+
+        public int LeftMostColumnWithOne(BinaryMatrix binaryMatrixParam)
+        {
+            _binaryMatrix = binaryMatrixParam;
+            _rows = _binaryMatrix.Dimensions()[0];
+            _cols = _binaryMatrix.Dimensions()[1];
+
+            var rowsWithOnes = GetRowsWithOnes();
+
+            foreach (var row in rowsWithOnes) BinarySearchRow(row);
+
+            if (_leftmostColumnWithOne == MaxMatrixLength + 1)
+                return -1;
+            return _leftmostColumnWithOne;
+        }
+
+        private HashSet<int> GetRowsWithOnes()
+        {
+            var rowsWithOnes = new HashSet<int>();
+            for (var row = 0; row < _rows; row++)
+                if (_binaryMatrix.Get(row, _cols - 1) == 1)
+                    rowsWithOnes.Add(row);
+            return rowsWithOnes;
+        }
+
+        private void BinarySearchRow(int row)
+        {
+            var left = 0;
+            var right = Math.Min(_leftmostColumnWithOne, _cols - 1);
+            if (_binaryMatrix.Get(row, right) == 0)
+                return;
+
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+                if (_binaryMatrix.Get(row, mid) == 1)
+                {
+                    _leftmostColumnWithOne = Math.Min(_leftmostColumnWithOne, mid);
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                    if (left >= _leftmostColumnWithOne)
+                        break;
+                }
+            }
+        }
+    }
+}
