e31398effdbab582eab4633dabdc09128e0449f4
J D <zathrath03@gmail.com>
2022-12-31 15:45:42 -0800

    Exercise 4 implementation

--- ControlFlowExercises/Program.cs
+++ ControlFlowExercises/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,8 @@ namespace ControlFlowExercises
         {
             //Exercise1();
             //Exercise2();
-            Exercise3();
+            //Exercise3();
+            Exercise4();
         }
 
         private static void Exercise1()
@@ -55,6 +57,41 @@ namespace ControlFlowExercises
             var height = int.Parse(Console.ReadLine() ?? string.Empty);
 
             Console.WriteLine(width > height ? "The image is landscape" : "The image is portrait");
+        }
+
+        private static void Exercise4()
+        {
+            /* Your job is to write a program for a speed camera. For
+             * simplicity, ignore the details such as camera, sensors, etc and
+             * focus purely on the logic. Write a program that asks the user
+             * to enter the speed limit. Once set, the program asks for the
+             * speed of a car. If the user enters a value less than the speed
+             * limit, program should display Ok on the console. If the value
+             * is above the speed limit, the program should calculate the
+             * number of demerit points. For every 5km/hr above the speed
+             * limit, 1 demerit points should be incurred and displayed on the
+             * console. If the number of demerit points is above 12, the
+             * program should display License Suspended.
+             */
+            const short kphOverPerDemerit = 5;
+
+            Console.Write("Enter the speed limit: ");
+            var speedLimit = short.Parse(Console.ReadLine() ?? string.Empty);
+            Console.Write("Enter the speed of the car: ");
+            var carSpeed = short.Parse(Console.ReadLine() ?? string.Empty);
+            if (carSpeed > speedLimit)
+            {
+                var demerits = (short)((carSpeed - speedLimit) / kphOverPerDemerit);
+                Console.WriteLine($"That car has incurred {demerits} demerit points");
+                if (demerits > 12)
+                {
+                    Console.WriteLine("License Suspended");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ok");
+            }
         }
     }
 
