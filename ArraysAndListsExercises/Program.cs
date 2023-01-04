65951d663e74e562b9048147d12af0adabf62d6c
J D <zathrath03@gmail.com>
2023-01-03 19:26:54 -0800

    Implement Exercise2

--- ArraysAndListsExercises/Program.cs
+++ ArraysAndListsExercises/Program.cs
@@ -7,7 +7,7 @@ namespace ArraysAndListsExercises
     {
         private static void Main()
         {
-            Exercise1();
+            Exercise2();
         }
 
         private static void Exercise1()
@@ -46,6 +46,22 @@ namespace ArraysAndListsExercises
                     break;
             }
 
+        }
+
+        private static void Exercise2()
+        {
+            /* Write a program and ask the user to enter their name. Use an array to reverse the name and then store the
+             * result in a new string. Display the reversed name on the console.
+             */
+
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            var nameChars = name.ToCharArray();
+            
+            Array.Reverse(nameChars);
+
+            var newName = new string(nameChars);
+            Console.WriteLine($"{newName}");
         }
     }
 }
