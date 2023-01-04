683c363642afcf76aee69ac4b92dbd1cd8780dee
J D <zathrath03@gmail.com>
2023-01-03 19:12:55 -0800

    Implement Exercise1

---
+++ ArraysAndListsExercises/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndListsExercises
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Exercise1();
+        }
+
+        private static void Exercise1()
+        {
+            /* When you post a message on Facebook, depending on the number of people who like your post, Facebook displays
+             * different information.
+             * If no one likes your post, it doesn't display anything.
+             * If only one person likes your post, it displays: [Friend's Name] likes your post.
+             * If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
+             * If more than two people like your post, it displays: [Friend 1], [Friend 2] and [Number of Other People]
+             * others like your post.
+             * Write a program and continuously ask the user to enter different names, until the user presses Enter
+             * (without supplying a name). Depending on the number of names provided, display a message based on the above
+             * pattern.
+             */
+            var names = new List<string>();
+            while (true) {
+                Console.Write("Enter a name or press enter to finish: ");
+                var input = Console.ReadLine();
+                if (input == string.Empty) break;
+                names.Add(input);
+            }
+
+
+            switch (names.Count)
+            {
+                case 0: break;
+                case 1:
+                    Console.WriteLine($"{names[0]} likes your post.");
+                    break;
+                case 2:
+                    Console.WriteLine($"{names[0]} and {names[1]} like your post.");
+                    break;
+                default:
+                    Console.WriteLine($"{names[0]}, {names[1]}, and {names.Count - 2} others like your post.");
+                    break;
+            }
+
+        }
+    }
+}
