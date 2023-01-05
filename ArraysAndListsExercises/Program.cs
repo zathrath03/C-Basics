2254fa0f426f8083a48fb623afc7fba394437b7b
J D <zathrath03@gmail.com>
2023-01-04 16:17:42 -0800

    Implment Exercise5()

--- ArraysAndListsExercises/Program.cs
+++ ArraysAndListsExercises/Program.cs
@@ -7,7 +7,7 @@ namespace ArraysAndListsExercises
     {
         private static void Main()
         {
-            Exercise4();
+            Exercise5();
         }
 
         private static void Exercise1()
@@ -115,6 +115,32 @@ namespace ArraysAndListsExercises
             foreach (var number in numbers)
                 Console.Write(number);
             Console.WriteLine();
+        }
+
+        private static void Exercise5()
+        {
+            /* Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the
+             * list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try;
+             * otherwise, display the 3 smallest numbers in the list.
+             */
+            const string message = "Enter a list of comma separated numbers (e.g 5, 1, 9, 2, 10): ";
+
+            Console.Write(message);
+            var input = Console.ReadLine().Split(',');
+
+            while (input.Length < 5)
+            {
+                Console.WriteLine("Invalid List"); // should be more descriptive - problem statement is specific though
+                Console.Write(message);
+                input = Console.ReadLine().Split(',');
+            }
+
+            Array.Sort(input);
+            for (var i = 0; i < 3; i++)
+            {
+                Console.Write(input[i]);
+                Console.Write(Convert.ToInt32(input[i]));
+            }
         }
     }
 }
