7a64d1ea477eecedfb3f90712e7c2c6f0fd2e299
J D <zathrath03@gmail.com>
2023-01-07 15:41:58 -0800

    Implement Exercise1()

--- WorkingWithFilesExercises/Program.cs
+++ WorkingWithFilesExercises/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,16 @@ namespace WorkingWithFilesExercises
 {
     internal class Program
     {
+        private const string Path = @"..\..\LoremIpsum.txt";
         static void Main(string[] args)
         {
+            Exercise1();
+        }
+
+        static void Exercise1()
+        {
+            // Write a program that reads a text file and displays the number of words.
+            Console.WriteLine(File.ReadAllText(Path).Split(' ').Length);
         }
     }
 }
