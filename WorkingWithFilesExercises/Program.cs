7e7048c80836373d8b0d4585c7a36b7d9709e80c
J D <zathrath03@gmail.com>
2023-01-07 15:53:02 -0800

    Cleanup unused usings and parameters; update all methods to private

--- WorkingWithFilesExercises/Program.cs
+++ WorkingWithFilesExercises/Program.cs
@@ -1,27 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WorkingWithFilesExercises
 {
     internal class Program
     {
         private const string Path = @"..\..\LoremIpsum.txt";
-        static void Main(string[] args)
+
+        private static void Main()
         {
             Exercise2();
         }
 
-        static void Exercise1()
+        private static void Exercise1()
         {
             // Write a program that reads a text file and displays the number of words.
             Console.WriteLine(File.ReadAllText(Path).Split(' ').Length);
         }
 
-        static void Exercise2()
+        private static void Exercise2()
         {
             // Write a program that reads a text file and displays the longest word in the file.
             var maxLength = 0;
